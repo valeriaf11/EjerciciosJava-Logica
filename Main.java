8238549672e337d8f88884b53172c2ca65f5c517
valeria tejada <you@example.com>
2025-06-02 11:50:48 -0600

    suma

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+public static void main(String[] args){
+    Suma suma = new Suma();
+        suma.suma(8, 14);
+}
+}
---
+++ Suma.java
@@ -0,0 +1,6 @@
+public class Suma {
+void suma (int a, int b) {
+    int suma = a + b;
+    System.out.println("El resultado de la operacion es:" +suma);
+}
+}
